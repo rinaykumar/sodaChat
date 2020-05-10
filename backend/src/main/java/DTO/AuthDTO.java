430e6c2852e1588fb0fd5433a2bd18241833b74f
rinaykumar <rinaykumar@gmail.com>
2020-05-10 01:34:51 -0700

    Ported over Mike's login/register

--- backend/src/main/java/DTO/AuthDTO.java
+++ backend/src/main/java/DTO/AuthDTO.java
@@ -1,4 +1,11 @@
 package DTO;
 
 public class AuthDTO {
+    public final String username;
+    public final String password;
+
+    public AuthDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
--- backend/src/main/java/Server/Server.java
+++ backend/src/main/java/Server/Server.java
@@ -3,6 +3,8 @@ package Server;
 import static spark.Spark.*;
 import DAO.MessagesDAO;
 import DTO.AddMessageDTO;
+import DTO.AuthDTO;
+import DTO.AuthResponseDTO;
 import DTO.MessagesListDTO;
 import com.google.gson.Gson;
 import com.mongodb.MongoClient;
@@ -22,6 +24,7 @@ public class Server {
     MongoDatabase db = mongoClient.getDatabase("FinalDatabase");
     // Get ref to collection
     MongoCollection<Document> messageCollection = db.getCollection("Messages");
+    MongoCollection<Document> userCollection = db.getCollection("Users");
 
     // Adds a second initial message to db if it's not already there
     List<Document> initialMessage2 = messageCollection.find(new Document("user", "User Name 2"))
@@ -59,28 +62,48 @@ public class Server {
     webSocket("/ws", WebSocketHandler.class); // open socket and leave it open
     get("/hello", (req, res) -> "hi"); // test
 
-    /*
-    post("/api/authenticate", (req, res) -> {
-      String bodyString =   req.body();
+    post("api/authenticate",(req, res) -> {
+      String bodyString = req.body();
       AuthDTO authDTO = gson.fromJson(bodyString, AuthDTO.class);
-      List<Document> potentialUser = userCollection.find(new Document("username", authDTO.username))
+
+      List<Document> user = userCollection.find(new Document("username", authDTO.username))
               .into(new ArrayList<>());
-      if (potentialUser.size() != 1) {
-        AuthResponseDTO responseDTO =
-                new AuthResponseDTO(false, "User not found");
+      if(user.size() != 1) {
+        AuthResponseDTO responseDTO = new AuthResponseDTO(false, "User not found");
         return gson.toJson(responseDTO);
       }
-      Document userDocument = potentialUser.get(0);
-      if (!userDocument.getString("password").equals(authDTO.password)) {
-        AuthResponseDTO responseDTO =
-                new AuthResponseDTO(false, "Password is incorrect");
+
+      Document userDocument = user.get(0);
+      if(!userDocument.getString("password").equals(authDTO.password)) {
+        AuthResponseDTO responseDTO = new AuthResponseDTO(false, "Password is incorrect");
         return gson.toJson(responseDTO);
       }
-      AuthResponseDTO responseDTO =
-              new AuthResponseDTO(true, null);
+
+      AuthResponseDTO responseDTO = new AuthResponseDTO(true, null);
       return gson.toJson(responseDTO);
+
     });
-     */
+
+    post("api/register", (req, res) -> {
+      String bodyString = req.body();
+      AuthDTO authDTO = gson.fromJson(bodyString, AuthDTO.class);
+
+      List<Document> user = userCollection.find(new Document("username", authDTO.username))
+              .into(new ArrayList<>());
+
+      if(!user.isEmpty()) {
+        AuthResponseDTO authResponseDTO = new AuthResponseDTO(false, "User already exists");
+        return gson.toJson(authResponseDTO);
+      }
+
+      Document newUser = new Document()
+              .append("username", authDTO.username)
+              .append("password", authDTO.password);
+      userCollection.insertOne(newUser);
+      AuthResponseDTO authResponseDto = new AuthResponseDTO(true, null);
+
+      return gson.toJson(authResponseDto);
+    });
 
     post("/api/addMessage", (req, res) -> {
       String bodyString = req.body();
