421b8d7101c22f303d7d0213e22f2b430cc0ce68
rinaykumar <rinaykumar@gmail.com>
2020-05-11 05:26:21 -0700

    Fixed issue with profile pics front

--- frontend/src/pages/Chatroom.js
+++ frontend/src/pages/Chatroom.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import '../css/stylesheet.css';
+import { Redirect } from 'react-router-dom';
+import '../css/chatroom.css';
 import Selfie from '../svg/selfie.jpg';
 import Profile1 from '../svg/profile1.png';
 import Profile2 from '../svg/profile2.png';
@@ -8,36 +9,34 @@ import Profile4 from '../svg/profile4.png';
 import LikeBtn from '../svg/like-bttn.svg';
 import axios from 'axios';
 
-const Chatroom = ({appUser, setAppUser}) => {    
+const Chatroom = ({ appUser, setAppUser }) => {
     const [message, setMessage] = React.useState('');
     const [messages, setMessages] = React.useState([]);
     const [profileNum, setProfileNum] = React.useState('');
-
-    // Setting username for testing until login auth is implemented
-    // setAppUser('user');
-
+    
     const fetchMessages = () => {
         axios.get('/api/getAllMessages')
-          .then((res) => {
-            console.log(res);
-            setMessages(res.data.messages);
-          })
-          .catch(console.log);
+            .then((res) => {
+                console.log(res);
+                setMessages(res.data.messages);
+            })
+            .catch(console.log);
     };
 
-    const ScrollMessages = ({messages}) => {
+    const ScrollMessages = ({ messages }) => {
         const lastMessageRef = React.useRef(null);
         const scrolltoBottom = () => {
-            lastMessageRef.current.scrollIntoView({  behavior: "smooth", duration : 10000, spy : "true"});
+            lastMessageRef.current.scrollIntoView({ behavior: "smooth", duration: 10000, spy: "true" });
         }
         React.useEffect(scrolltoBottom, [messages]);
-        return(
+        return (
             <div>
-                <div ref={lastMessageRef}/>
+                <div ref={lastMessageRef} />
             </div>
         );
     }
 
+    // This grabs the current user's profile pic number for the sidebar
     const profilePic = () => {
         axios.post('/api/profilePic', appUser)
             .then((res) => {
@@ -45,38 +44,38 @@ const Chatroom = ({appUser, setAppUser}) => {
                 setProfileNum(res.data);
             })
             .catch(console.log);
-         
+
         switch (profileNum) {
-            case 1: 
+            case 0:
                 return Profile1
-            case 2:
+            case 1:
                 return Profile2
-            case 3:
+            case 2:
                 return Profile3
             default:
-                return Profile4    
-        }    
+                return Profile4
+        }
     }
 
-    const submitMessage = () => { 
+    const submitMessage = () => {
         console.log("From submitMessage");
         console.log(message);
         console.log(appUser);
         const body = {
-          text: message,
-          user: appUser
+            text: message,
+            user: appUser
         };
         axios.post('/api/addMessage', body)
-          .then(() => setMessage(''))
-          .then(() => fetchMessages())
-          .catch(console.log);
-      };
+            .then(() => setMessage(''))
+            .then(() => fetchMessages())
+            .catch(console.log);
+    };
 
     const parseText = (message) => {
         let obj = JSON.parse(message);
         return obj.text;
     }
-    
+
     const parseUser = (message) => {
         let obj = JSON.parse(message);
         return obj.user;
@@ -86,92 +85,113 @@ const Chatroom = ({appUser, setAppUser}) => {
         let obj = JSON.parse(message);
         return obj.date;
     }
-      
+
+    // This sets the profile pic for loaded messages and new messages
+    const parsePic = (message) => {
+        let obj = JSON.parse(message);
+        console.log("picNum:")
+        console.log(obj.picNum)
+        switch (obj.picNum) {
+            case "0":
+                return Profile1
+            case "1":
+                return Profile2
+            case "2":
+                return Profile3
+            case "3":
+                return Profile4
+        }
+    }
+
     React.useEffect(() => {
         fetchMessages();
     }, []);
 
+    if (!appUser) {
+        return <Redirect to="/" />;
+    }
+
     return (
-    <div>
-        <div class="flexbox">
-            <div id="sidebar">
-                <div id="dismiss">
-                    <i class="fas fa-arrow-left"></i>
-                </div>
-                <div class="left-menu-bar">
-                    <div class="menu-profile-info">
-                        <img id="user-profile-image" src={profilePic()} alt=""/>
-                        {appUser && <h5 id="username">{appUser}</h5>} 
+        <div>
+            <div class="flexbox">
+                <div id="sidebar">
+                    <div id="dismiss">
+                        <i class="fas fa-arrow-left"></i>
                     </div>
-                    <div class="bottom-buttons">
-                        <button class="menu-buttons" id="profile-bttn" type="button" name="profile">PROFILE</button>
-                        <button class="menu-buttons" id="logout-bttn" type="button" name="logout">LOGOUT</button>
+                    <div class="left-menu-bar">
+                        <div class="menu-profile-info">
+                            <img id="user-profile-image" src={profilePic()} alt="" />
+                            {appUser && <h5 id="username">{appUser}</h5>}
+                        </div>
+                        <div class="bottom-buttons">
+                            <button class="menu-buttons" id="profile-bttn" type="button" name="profile">PROFILE</button>
+                            <button class="menu-buttons" id="logout-bttn" type="button" name="logout">LOGOUT</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div id="content">
-                <button type="button" id="sidebarCollapse" class="btn btn-info">
-                    <i class="fas fa-align-left"></i>
-                </button>
-                <div class="right-chatroom">
-                    <div class="messages-box">
-                        <div class="submitted-message">
-                            <div class="submitted-message-box-1">
-                                <img class="submitted-message-image" src={Selfie} alt=""/>
-                            </div>
-                            <div class="submitted-message-box-2">
-                                <div class="message-content-header">
-                                    <h5 id="username-content">User Name 1</h5>
-                                    <p id="date-content">May 5, 2020, 9:25:23 PM</p>
+                <div id="content">
+                    <button type="button" id="sidebarCollapse" class="btn btn-info">
+                        <i class="fas fa-align-left"></i>
+                    </button>
+                    <div class="right-chatroom">
+                        <div class="messages-box">
+                            <div class="submitted-message">
+                                <div class="submitted-message-box-1">
+                                    <img class="submitted-message-image" src={Selfie} alt="" />
                                 </div>
-                                <div class="message-content-text">
-                                    <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                    laboris nisi ut aliquip ex ea commodo consequat.
-                                    </p>
-                                    <button id="like-button" type="button" name="like-button">
-                                    <img id="thumbs-up" src={LikeBtn} alt=""/> 
-                                    <p id="thumbs-up-count">0</p>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>  
-                        {messages.map((message) => {
-                            return (
-                                <div class="submitted-message">
-                                    <div class="submitted-message-box-1">
-                                        <img class="submitted-message-image" src={profilePic()} alt=""/>
+                                <div class="submitted-message-box-2">
+                                    <div class="message-content-header">
+                                        <h5 id="username-content">User Name 1</h5>
+                                        <p id="date-content">May 5, 2020, 9:25:23 PM</p>
                                     </div>
-                                    <div class="submitted-message-box-2">
-                                        <div class="message-content-header">
-                                            <h5 id="username-content">{parseUser(message)}</h5>
-                                            <p id="date-content">{parseDate(message)}</p>
-                                        </div>
-                                        <div class="message-content-text">
-                                            <p>{parseText(message)}</p>
-                                            <button id="like-button" type="button" name="like-button">
-                                                <img id="thumbs-up" src={LikeBtn} alt=""/> 
-                                                <p id="thumbs-up-count">0</p>
-                                            </button>
-                                        </div>                                        
-                                    </div> 
-                                    <ScrollMessages messages={messages}/> 
+                                    <div class="message-content-text">
+                                        <p>
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                                            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                            laboris nisi ut aliquip ex ea commodo consequat.
+                                    </p>
+                                        <button id="like-button" type="button" name="like-button">
+                                            <img id="thumbs-up" src={LikeBtn} alt="" />
+                                            <p id="thumbs-up-count">0</p>
+                                        </button>
+                                    </div>
                                 </div>
-                            ); 
-                        })}
+                            </div>
+                            {messages.map((message) => {
+                                return (
+                                    <div class="submitted-message">
+                                        <div class="submitted-message-box-1">
+                                            <img class="submitted-message-image" src={parsePic(message)} alt="" />
+                                        </div>
+                                        <div class="submitted-message-box-2">
+                                            <div class="message-content-header">
+                                                <h5 id="username-content">{parseUser(message)}</h5>
+                                                <p id="date-content">{parseDate(message)}</p>
+                                            </div>
+                                            <div class="message-content-text">
+                                                <p>{parseText(message)}</p>
+                                                <button id="like-button" type="button" name="like-button">
+                                                    <img id="thumbs-up" src={LikeBtn} alt="" />
+                                                    <p id="thumbs-up-count">0</p>
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <ScrollMessages messages={messages} />
+                                    </div>
+                                );
+                            })}
+                        </div>
+
+                        <div class="send-messages-box">
+                            <input id="send-message-input" placeholder="Message #general" name="message" value={message} onChange={e => setMessage(e.target.value)}></input>
+                            <button id="send-message-button" type="button" name="send-message" onClick={submitMessage}>SEND</button>
+                        </div>
                     </div>
-                    
-                    <div class="send-messages-box">
-                        <input id="send-message-input" placeholder="Message #general" name="message" value={message} onChange={e => setMessage(e.target.value)}></input>
-                        <button id="send-message-button" type="button" name="send-message" onClick={submitMessage}>SEND</button>
-                    </div>
-                </div>    
+                </div>
             </div>
+            <div class="overlay"></div>
         </div>
-        <div class="overlay"></div>
-    </div>    
     );
-  }
-  
-  export default Chatroom;
+}
+
+export default Chatroom;
