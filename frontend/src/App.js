185cb998f4553113a93cc6206ac823afa7e7ae1b
rinaykumar <rinaykumar@gmail.com>
2020-05-12 05:15:11 -0700

    Alternative styling for login and signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,36 +8,15 @@ import Login from "./pages/Login";
 const App = () => {
   const [appUser, setAppUser] = React.useState('');
 
-<<<<<<< HEAD
-  // logs out the [USER]
-  const logout = (e) => {
-    setAppUser(null);
-  }
-
-=======
   // Commenting out for now
   // logs out the [USER]
   // const logout = (e) => {
   //   setAppUser(null); 
   // }
->>>>>>> 563eb1dc158ee346b8ba558566d3a9c48bd35645
 
   return (
     <div>
       <BrowserRouter>
-<<<<<<< HEAD
-        <Switch>
-          <Route path="/chatroom">
-            <Chatroom appUser={appUser} setAppUser={setAppUser} />
-          </Route>
-          <Route path="/signup">
-            <Signup appUser={appUser} setAppUser={setAppUser} />
-          </Route>
-          <Route path="/">
-            <Login appUser={appUser} setAppUser={setAppUser} />
-          </Route>
-        </Switch>
-=======
       <Switch>
         <Route path="/chatroom">
           <Chatroom appUser={appUser} setAppUser={setAppUser} />
@@ -49,7 +28,6 @@ const App = () => {
           <Login appUser={appUser} setAppUser={setAppUser} />
         </Route>
       </Switch>
->>>>>>> 563eb1dc158ee346b8ba558566d3a9c48bd35645
       </BrowserRouter>
     </div>
   );
