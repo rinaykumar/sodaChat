d3b6eb37cf28a6cdcc1749ec9d7376373a1eb5ad
Girish Rawat <emailgirishrawat@gmail.com>
2020-05-12 20:53:55 -0700

    added fields to profile page

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,28 +1,69 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
+import React from "react";
+import { Redirect } from "react-router-dom";
 
-import Selfie from '../svg/selfie.jpg';
-import Profile1 from '../svg/profile1.png';
-import Profile2 from '../svg/profile2.png';
-import Profile3 from '../svg/profile3.png';
-import Profile4 from '../svg/profile4.png';
-import LikeBtn from '../svg/like-bttn.svg';
-import axios from 'axios';
-import '../css/profile.css';
-
+import Selfie from "../svg/selfie.jpg";
+import Profile1 from "../svg/profile1.png";
+import Profile2 from "../svg/profile2.png";
+import Profile3 from "../svg/profile3.png";
+import Profile4 from "../svg/profile4.png";
+import LikeBtn from "../svg/like-bttn.svg";
+import axios from "axios";
+import "../css/profile.css";
 
 const Profile = ({ appUser, setAppUser }) => {
-    const [username, setUsername] = React.useState('');
-    const [password, setPassword] = React.useState('');
-    const [error, setError] = React.useState('');
-    const [usertype, setUserType] = React.useState('');
-    const [profileNum, setProfileNum] = React.useState('');
- 
-    return (
-      <div>
-          <h1>Profile</h1>
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [error, setError] = React.useState("");
+  const [usertype, setUserType] = React.useState("");
+  const [profileNum, setProfileNum] = React.useState("");
+
+  const profilePic = () => {
+    axios.post('/api/profilePic', appUser)
+        .then((res) => {
+            console.log(res.data);
+            setProfileNum(res.data);
+        })
+        .catch(console.log);
+
+    switch (profileNum) {
+        case 0:
+            return Profile1
+        case 1:
+            return Profile2
+        case 2:
+            return Profile3
+        default:
+            return Profile4
+    }
+}
+
+  return (
+    <div>
+
+      <div className="flexContainerRow">
+        <div className="flexBox-1">
+          <br />
+          <br />
+          <div
+            id="signInBox"
+            className="AccountBox centerBoxH flexContainerCol fontRobotoMono"
+          >
+            <div className="flexBox-2">
+            <img id="user-profile-image" src={profilePic()} alt="" />
+            {appUser && <h5 id="username">{appUser}          <br />
+          <br /></h5>}
+            <p class="centerText">Username:
+                <input className="username-input inputText" value={appUser} onChange={(e) => setUsername(e.target.value)} />
+              </p>
+              <p class="centerText">Password:
+                <input className="password-input inputText" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default Profile;
