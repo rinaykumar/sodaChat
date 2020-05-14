4a8afc6e99cf5a4c9ce19d0726bd8b2e6f47f8d4
Jose H Gonzalez <gonzalezhilariojose@gmail.com>
2020-05-14 15:06:10 -0700

    DTO changed

--- backend/src/main/java/DTO/AuthDto.java
+++ backend/src/main/java/DTO/AuthDto.java
@@ -1,13 +1,13 @@
 package DTO;
 
-public class AuthDto
-{
+public class AuthDTO {
     public final String username;
     public final String password;
+    public final int profilePic;
 
-    public AuthDto(String username, String password)
-    {
+    public AuthDTO(String username, String password, int profilePic) {
         this.username = username;
         this.password = password;
+        this.profilePic = profilePic;
     }
 }
--- backend/src/main/java/DTO/AuthResponseDto.java
+++ backend/src/main/java/DTO/AuthResponseDto.java
@@ -1,12 +1,10 @@
 package DTO;
 
-public class AuthResponseDto
-{
+public class AuthResponseDTO {
     public final Boolean success;
     public final String error;
 
-    public AuthResponseDto(Boolean success, String error)
-    {
+    public AuthResponseDTO(Boolean success, String error) {
         this.success = success;
         this.error = error;
     }
