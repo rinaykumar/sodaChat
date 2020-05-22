08bc251426b379e23910af0cfec7a8c92d515979
Girish Rawat <Emailgirishrawat@gmail.com>
2020-05-21 22:30:11 -0700

    Update Profile.js

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
+import { Redirect, withRouter } from "react-router-dom";
 
 import Selfie from "../svg/selfie.jpg";
 import Profile1 from "../svg/profile1.png";
@@ -10,13 +10,14 @@ import LikeBtn from "../svg/like-bttn.svg";
 import axios from "axios";
 import "../css/profile.css";
 
-const Profile = ({ appUser, setAppUser }) => {
+const Profile  = ({ appUser, setAppUser }) => {
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [Newusername, setNewUsername] = React.useState("");
   const [error, setError] = React.useState("");
   const [usertype, setUserType] = React.useState("");
   const [profileNum, setProfileNum] = React.useState("");
+  const [redirect, setRedirect] = React.useState("");
 
   const deleteUser = () => {
     console.log(appUser);
@@ -38,6 +39,11 @@ const Profile = ({ appUser, setAppUser }) => {
       .catch(() => {
         setError("Failed to delete");
       });
+      alert("User deleted.")
+      if(appUser){
+        setAppUser(null)
+        setRedirect(true);
+       }
   };
 
   const changeName = () => {
@@ -60,6 +66,11 @@ const Profile = ({ appUser, setAppUser }) => {
       .catch(() => {
         setError("Failed to change");
       });
+      alert("Username Changed.")
+      if(appUser){
+        setAppUser(null)
+        setRedirect(true);
+       }
   };
 
   const changepassword = () => {
@@ -73,8 +84,9 @@ const Profile = ({ appUser, setAppUser }) => {
       .then((res) => {
         console.log(res.data);
         if (res.data.success) {
-          console.log("Worked!");
+          console.log("yeah password is changed!");
           setAppUser(username);
+          
         } else {
           setError(res.data.error);
         }
@@ -82,7 +94,26 @@ const Profile = ({ appUser, setAppUser }) => {
       .catch(() => {
         setError("Failed to change");
       });
+
+      alert("Password Changed.")
+      if(appUser){
+        setAppUser(null)
+        setRedirect(true);
+       }
   };
+
+  const renderRedirect = () => {
+    if (redirect) {
+      return <Redirect to='/' />
+    }
+  }
+
+  const logout = () => {
+    if(appUser){
+      setAppUser(null)
+      setRedirect(true);
+     }
+  }
 
   const profilePic = () => {
     axios.post('/api/profilePic', appUser)
@@ -106,36 +137,39 @@ const Profile = ({ appUser, setAppUser }) => {
 
   return (
     <div>
-
+{renderRedirect()}
       <div className="flexContainerRow">
         <div className="flexBox-1">
           <br />
           <br />
           <div
-            id="signInBox"
-            className="AccountBox centerBoxH flexContainerCol fontRobotoMono"
+            // id="signInBox"
+            // className="AccountBox centerBoxH flexContainerCol fontRobotoMono"
           >
             <div className="flexBox-2">
             <img id="user-profile-image" src={profilePic()} alt="" />
-            {appUser && <h5 id="username">{appUser}          <br />
+            {appUser && <h5 id="username">{appUser}<br />
           <br /></h5>}
             <p class="centerText">Username:
                 <input className="username-input inputText" value={appUser} onChange={(e) => setUsername(e.target.value)} />
               </p>
-              <p class="centerText">NewUsername:
+              <p class="centerText">New Username:
                 <input className="password-input inputText"  value={Newusername} onChange={(e) => setNewUsername(e.target.value)} />
               </p>
-              <p class="centerText">NewPassword:
+              <p class="centerText">New Password:
                 <input className="password-input inputText" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
               </p>
               <p class="centerText">
-              <button class="fontRobotoMono" onClick={deleteUser}>delete user</button>
+              <button class="fontRobotoMono" onClick={deleteUser}>Delete User</button>
               </p>
               <p class="centerText">
-              <button class="fontRobotoMono" onClick={changeName}>change username</button>
+              <button class="fontRobotoMono" onClick={changeName}>Change Username</button>
               </p>
               <p class="centerText">
-              <button class="fontRobotoMono" onClick={changepassword}>change password</button>
+              <button class="fontRobotoMono" onClick={changepassword}>Change Password</button>
+              </p>
+              <p class="centerText">
+              <button class="fontRobotoMono" onClick={logout}>Logout</button>
               </p>
             </div>
           </div>
